8a3e4d4e959ca5daeccd70e41919d63f948cb8ca
Parnit Sainion <tinrap@users.noreply.github.com>
2014-08-06 19:45:26 -0700

    test commit

--- src/com/woapp/MainActivity.java
+++ src/com/woapp/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		next = (Button) findViewById(R.id.next);
